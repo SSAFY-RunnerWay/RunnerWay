91b212393f32fb77ab345342f2ae1b7c48ef78c4
장준석 <angec5689@naver.com>
2024-09-09 10:39:14 +0900

    feat: Init BackEnd Project

---
+++ BackEnd/src/main/java/chuchu/runnerway/RunnerwayApplication.java
@@ -0,0 +1,13 @@
+package chuchu.runnerway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RunnerwayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RunnerwayApplication.class, args);
+	}
+
+}
---
+++ BackEnd/src/test/java/chuchu/runnerway/RunnerwayApplicationTests.java
@@ -0,0 +1,13 @@
+package chuchu.runnerway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RunnerwayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
