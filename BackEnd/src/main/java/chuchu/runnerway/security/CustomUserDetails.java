5d15bacf7a240cc6d821624bce9a1d481003009a
장준석 <angec5689@naver.com>
2024-09-10 14:34:23 +0900

    feat: spring-security 적용

---
+++ BackEnd/src/main/java/chuchu/runnerway/security/CustomUserDetails.java
@@ -0,0 +1,45 @@
+package chuchu.runnerway.security;
+
+import chuchu.runnerway.member.dto.MemberDto;
+import java.util.Collection;
+import java.util.List;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+public record CustomUserDetails(MemberDto member) implements UserDetails {
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of();
+    }
+
+    @Override
+    public String getPassword() {
+        return "Use not password in our service";
+    }
+
+    @Override
+    public String getUsername() {
+        return Long.toString(member.getMemberId());
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
