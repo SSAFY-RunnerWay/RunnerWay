f0e3cdf4fdd43c258fd8ae33734cdd8f2c5dc621
SeongJeong <jsj046@naver.com>
2024-09-20 16:34:48 +0900

    feat: 검색 자동완성 기능

--- BackEnd/src/main/java/chuchu/runnerway/course/model/service/SearchCourseService.java
+++ BackEnd/src/main/java/chuchu/runnerway/course/model/service/SearchCourseService.java
@@ -1,8 +1,12 @@
 package chuchu.runnerway.course.model.service;
 
+import chuchu.runnerway.course.dto.response.MatchByWordResponseDto;
 import chuchu.runnerway.course.dto.response.SelectAllResponseDto;
+
+import java.util.List;
 
 public interface SearchCourseService {
     SelectAllResponseDto search(String searchWord, int page, int size);
+    List<MatchByWordResponseDto> matchByWord(String searchWord);
     void insertCourse();
 }
