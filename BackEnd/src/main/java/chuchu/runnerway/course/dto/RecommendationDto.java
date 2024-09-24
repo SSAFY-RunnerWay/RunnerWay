ab5ce7277a60acc1d3e0c9528793a167ab7f7386
이재민 <jml6534@naver.com>
2024-09-24 22:55:39 +0900

    feat: fastapi 연결

---
+++ BackEnd/src/main/java/chuchu/runnerway/course/dto/RecommendationDto.java
@@ -0,0 +1,9 @@
+package chuchu.runnerway.course.dto;
+
+import lombok.Getter;
+
+@Getter
+public class RecommendationDto {
+    private Long courseId;
+    private double recommendationScore;
+}
--- BackEnd/src/main/java/chuchu/runnerway/course/model/service/OfficialCourseServiceImpl.java
+++ BackEnd/src/main/java/chuchu/runnerway/course/model/service/OfficialCourseServiceImpl.java
@@ -1,23 +1,32 @@
 package chuchu.runnerway.course.model.service;
 
+import chuchu.runnerway.course.dto.RecommendationDto;
 import chuchu.runnerway.course.dto.response.OfficialDetailResponseDto;
 import chuchu.runnerway.course.dto.response.OfficialListResponseDto;
 import chuchu.runnerway.course.entity.Course;
 import chuchu.runnerway.course.mapper.CourseMapper;
 import chuchu.runnerway.course.model.repository.OfficialCourseRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.http.ResponseEntity;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 import java.util.*;
 import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class OfficialCourseServiceImpl implements OfficialCourseService{
+
+    private final WebClient webClient;
 
     private final OfficialCourseRepository officialCourseRepository;
     private final CourseMapper courseMapper;
@@ -26,6 +35,7 @@ public class OfficialCourseServiceImpl implements OfficialCourseService{
     @Override
     public List<OfficialListResponseDto> findAllOfiicialCourse(double lat, double lng) {
         List<Course> courses = officialCourseRepository.findAll(lat, lng);
+        getRecommendation();
         return courseMapper.toOfficialListResponseDtoList(courses);
     }
 
@@ -60,4 +70,60 @@ public class OfficialCourseServiceImpl implements OfficialCourseService{
 
     }
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    public void getRecommendation () {
+        Flux<RecommendationDto> dto = webClient.get()
+                .uri(uriBuilder -> uriBuilder
+                        .path("/recommendation")
+                        .queryParam("member_id", 13)
+                        .build())
+                .retrieve()
+                .bodyToFlux(RecommendationDto.class);
+
+        dto.subscribe(
+                recommendationDto -> {
+                    // 추천 DTO의 필드를 출력
+                    System.out.println("Course ID: " + recommendationDto.getCourseId());
+                    System.out.println("Recommendation Score: " + recommendationDto.getRecommendationScore());
+                },
+                error -> {
+                    // 에러 처리
+                    System.err.println("Error occurred: " + error.getMessage());
+                },
+                () -> {
+                    // 완료 시 호출되는 메소드
+                    System.out.println("Recommendation retrieval completed.");
+                }
+        );
+    }
 }
