a3b5566a1a813c8d3d385b35066152e903c8c22f
이지혜 <hyeee19@naver.com>
2024-09-25 13:26:12 +0900

    feat: runner pick 페이지 필터 추가

--- frontend/lib/views/runnerPick/runner_pick_view.dart
+++ frontend/lib/views/runnerPick/runner_pick_view.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:frontend/views/base_view.dart';
+import 'package:frontend/widgets/filter_condition.dart';
 import 'package:frontend/widgets/search/search_read_only.dart';
 
 import '../search/widget/search_bar.dart';
@@ -14,7 +15,35 @@ class RunnerPickView extends StatelessWidget {
         padding: EdgeInsets.all(20),
         child: Column(
           children: [
+            // 검색바
             SearchReadOnly(),
+            SizedBox(height: 20),
+
+            // '러너들의 추천 코스' 제목
+            Row(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                SizedBox(
+                  width: 5,
+                ),
+                Text(
+                  '러너들의 ',
+                  style: TextStyle(fontWeight: FontWeight.w700, fontSize: 22),
+                ),
+                Text(
+                  '추천코스',
+                  style: TextStyle(
+                    fontWeight: FontWeight.w400,
+                    fontSize: 22,
+                  ),
+                ),
+              ],
+            ),
+
+            SizedBox(
+              height: 20,
+            ),
+            FilterCondition(),
           ],
         ),
       ),
