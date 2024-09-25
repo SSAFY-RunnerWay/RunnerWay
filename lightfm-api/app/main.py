c82a039d31f9811d8fdfd0fad891f04c377c54a1
이재민 <jml6534@naver.com>
2024-09-25 22:38:16 +0900

    fix: 파일 경로 수정

--- lightfm-api/app/main.py
+++ lightfm-api/app/main.py
@@ -5,18 +5,20 @@ from .models import Course as SQLACourse, Member as SQLAMember, CourseTags as SQ
 from .schema import Course, Member, CourseTags, FavoriteCourse  # Pydantic 스키마 임포트
 from.train_model import load_data
 import pandas as pd  # 데이터프레임 처리
+import os
 
 
 app = FastAPI()
 
-
 @app.get("/recommendation")
-def test(
+def getCourses(
     member_id: int = Query(...), 
     lat: float = Query(...),
     lng: float = Query(...),
     db: Session = Depends(get_db)
 ):
     # log_path = "C:/Users/SSAFY/Desktop/Project/runnerway/lightFM/lightfm-api/app/running_logs4.csv"
-    log_path = "./running_logs4.csv"
+    # log_path = "./running_logs4.csv"
+    BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+    log_path = os.path.join(BASE_DIR, "running_logs4.csv")
     return load_data(db, log_path, member_id, lat, lng)
