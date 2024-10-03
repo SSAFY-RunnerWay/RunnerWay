c0585c97e87dcf2b99f5d464f69343dbcd21fdb8
SeongJeong <jsj046@naver.com>
2024-10-03 15:02:45 +0900

    fix: RankingResponseDto id 추가

--- BackEnd/src/main/java/chuchu/runnerway/ranking/dto/response/RankingResponseDto.java
+++ BackEnd/src/main/java/chuchu/runnerway/ranking/dto/response/RankingResponseDto.java
@@ -3,11 +3,11 @@ package chuchu.runnerway.ranking.dto.response;
 import chuchu.runnerway.ranking.dto.reference.RankerMemberDto;
 import lombok.Data;
 
-import java.sql.Time;
 import java.time.LocalTime;
 
 @Data
 public class RankingResponseDto {
+    private Long rankId;
     private LocalTime score;
     private RankerMemberDto memberDto;
 }
