cdcbd9fc92a1cd4dc3269012c47bebe86ff37bc1
Lee soyeon <168151572+lso401@users.noreply.github.com>
2024-10-06 12:41:36 +0900

    feat : 공식 코스 기록 상세 모두 불러오기 기능 구현

--- frontend/lib/views/running/record_detail_view.dart
+++ frontend/lib/views/running/record_detail_view.dart
@@ -106,17 +106,25 @@ class RecordDetailView extends StatelessWidget {
                     // TODO distance이면 0.0 으로 불러와져서 courseLength로 둠.
                     // 확인 필요
                     ReviewRecordItem(
-                      value:
-                          recordController.courseDetail.value?.courseLength ??
-                              0.0,
+                      value: record.runningDistance ?? 0.0,
                       label: '운동 거리',
                     ),
+                    // 운동 시간 (finishDate - startDate)
+                    // 운동 시간 (초 단위로 계산 후 전달)
                     ReviewRecordItem(
-                      value: record.courseId, // 운동 시간 값으로 수정 필요
+                      value: (record.finishDate != null &&
+                              record.startDate != null)
+                          ? DateTime.parse(record.finishDate!)
+                              .difference(DateTime.parse(record.startDate!))
+                              .inSeconds // 초 단위로 계산하여 전달
+                          : 0,
                       label: '운동 시간',
                     ),
+                    // TODO 러닝 경사도 이거 맞나
                     ReviewRecordItem(
-                      value: record.courseId, // 경사도 값으로 수정 필요
+                      value:
+                          recordController.courseDetail.value?.averageSlope ??
+                              0.0,
                       label: '러닝 경사도',
                     ),
                     ReviewRecordItem(
